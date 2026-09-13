13f76e1d48bc83c24a2271b4921f4a44fd484661
Etregin <146861227+Etregin@users.noreply.github.com>
2026-09-13 10:41:43 +0300

    build(android): add proguard rules and configure r8 for release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,6 +73,10 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
